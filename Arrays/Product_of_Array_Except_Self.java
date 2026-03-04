43ef4f19d9a708f036b34e16f9779954e8310ac1
hemanthh2005 <hemanthkumar34174@gmail.com>
2026-03-04 21:42:38 +0530

    Add files via upload

---
+++ Arrays/Product_of_Array_Except_Self.java
@@ -0,0 +1,39 @@
+package Hemanth;
+
+//https://leetcode.com/problems/product-of-array-except-self/description/
+//238
+
+import java.util.Arrays;
+
+public class Product_of_Array_Except_Self {
+    public static void main(String[] args) {
+        int [] nums={1,2,3,4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int n=nums.length;
+        int [] result=new int[n];
+        int [] prefix=new int[n];
+        int [] suffix=new int[n];
+
+        prefix[0]=1;
+
+        for (int i = 1; i <n ; i++) {
+            prefix[i]=prefix[i-1]*nums[i-1];
+        }
+
+        suffix[n-1]=1;
+        for (int i = n-2; i >=0 ; i--) {
+            suffix[i]=suffix[i+1]*nums[i+1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[i]=prefix[i]*suffix[i];
+        }
+
+        return result;
+    }
+}
+
