28bb973e35f98cdf1893b12aff3e991a69c7fb97
hemanthh2005 <hemanthkumar34174@gmail.com>
2026-03-08 22:24:22 +0530

    Add files via upload

---
+++ Arrays/spiral_matrix.java
@@ -0,0 +1,58 @@
+package Hemanth;
+
+//https://leetcode.com/problems/spiral-matrix/
+//54
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class spiral_matrix {
+    public static void main(String[] args) {
+        int [][] mat={{1,2,3},
+                {4,5,6},
+                {7,8,9},
+        };
+
+        System.out.println(spiralOrder(mat));
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> result = new ArrayList<>();
+
+        int top=0;
+        int bottom=matrix.length-1;
+
+        int left=0;
+        int right=matrix[0].length-1;
+
+        while (top<=bottom && left<=right){
+            for (int i = left; i <=right ; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <=bottom ; i++) {
+
+                result.add(matrix[i][right]);
+
+            }
+            right--;
+
+            if (top<=bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left<=right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+
+        }
+            return result;
+    }
+}
