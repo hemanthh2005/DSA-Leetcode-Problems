d387b69a21e7a1d270eb41729a418911c518160c
Hemanth Kumar <hemanthkumar34174@gmail.com>
2026-08-31 23:02:17 +0530

    Add files via upload

---
+++ String/ZigZag_string.java
@@ -0,0 +1,42 @@
+package Leetcode150;//https://leetcode.com/problems/zigzag-conversion/
+//6
+
+class ZigZag_string {
+    public static String convert(String s, int numRows) {
+
+        if(numRows==1 || numRows>=s.length()){
+            return s;
+        }
+
+        StringBuilder [] rows=new StringBuilder[numRows];
+
+        for(int i=0;i<numRows;i++){
+            rows[i]=new StringBuilder();
+        }
+
+        int currentRow=0;
+        boolean goingDown=false;
+
+        for(char c:s.toCharArray()){
+            rows[currentRow].append(c);
+
+            if(currentRow==0 || currentRow==numRows-1){
+                goingDown=!goingDown;
+            }
+            currentRow+=goingDown?1:-1;
+        }
+
+        StringBuilder result =new StringBuilder();
+
+        for(StringBuilder row:rows){
+            result.append(row);
+        }
+
+        return result.toString();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert("PAYPALISHIRING",3));
+    }
+}
